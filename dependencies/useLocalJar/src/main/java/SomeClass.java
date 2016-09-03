fa91746b42b1929baa62c0209de2dc272ba74420
a <ruan65@gmail.com>
2016-09-04 01:44:03 +0300

    changes

---
+++ dependencies/useLocalJar/src/main/java/SomeClass.java
@@ -0,0 +1,11 @@
+public class SomeClass {
+
+    public boolean someLibraryMethod() {
+        return true;
+    }
+
+    public static void main(String[] args) {
+
+        new Library().sayHi();
+    }
+}
---
+++ plugins/java1/src/main/java/com/ruan/java1/Person.java
@@ -0,0 +1,18 @@
+package com.ruan.java1;
+
+public class Person {
+	
+    private final String name;
+
+    public Person(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public static void main(String[] args){
+        System.out.println("Hello from Java");
+    }
+}
